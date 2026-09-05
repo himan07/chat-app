64f54e1b962d9c5eed482ddc214b90351be2509f
himan07 <himan9714@gmail.com>
2026-09-05 19:10:37 +0530

    added exit

--- Client.java
+++ Client.java
@@ -55,6 +55,11 @@ import java.io.*;
                     String content = br1.readLine();
                     out.println(content);
                     out.flush();
+
+                    if(content.equals("exit")){
+                        socket.close();
+                        break;
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
--- Server.java
+++ Server.java
@@ -30,10 +30,11 @@ class Server {
         Runnable r1 = () -> {
             System.out.println("Reader started...");
             try {
-            while (true) {
+                while (true) {
                     String msg = br.readLine();
                     if (msg == null || msg.equals("exit")) {
                         System.out.println("Client has terminated the chat session...");
+                        socket.close();
                         break;
                     }
 
@@ -52,11 +53,16 @@ class Server {
         Runnable r2 = () -> {
             System.out.println("Writer started...");
             try {
-            while (true) {
+                while (true) {
                     BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
                     String content = br1.readLine();
                     out.println(content);
                     out.flush();
+
+                    if (content.equals("exit")) {
+                        socket.close();
+                        break;
+                    }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
